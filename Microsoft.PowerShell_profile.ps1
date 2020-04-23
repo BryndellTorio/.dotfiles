aaaee558df6cc15d2e6935ef97f31d6949b15580
Bryndell <bryndelltorio@gmail.com>
2020-04-23 14:25:56 +0400

    New Alias in PowerShell for faster coding

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+set-location c:\
+function prjdir { cd C:\Users\Rndpc13\Desktop\Project\ }
+ Set-Alias shortcut `prjdir`
+function toff { shutdown /p }
+Set-Alias shortcut 'toff'
+function reset { shutdown /r }
+Set-Alias shortcut 'reset'
