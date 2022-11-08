e61c1704b0d54dea3efd405b0eefc6df09480699
Engr. Bryndell Torio <bryndell.torio@global-imi.com>
2022-11-08 09:02:19 +0800

    CREATE SIMILAR TO create_project_directory.bat using python. Give to junior engineers.

---
+++ powershell/create_project_directory.bat
@@ -0,0 +1,30 @@
+echo Use this to create an initial Project directory
+REM replace DSSTI02 w/ the project code
+
+mkdir DSSTI02_Documents
+cd DSSTI02_Documents
+mkdir DSSTI02_BOM
+mkdir DSSTI02_DFM_Docs
+mkdir DSSTI02_Datasheets
+cd..
+mkdir DSSTI02_Design_Files
+cd DSSTI02_Design_Files
+mkdir DSSTI02_Layout
+cd DSSTI02_Layout
+mkdir DSSTI02_Gerber_Files
+cd DSSTI02_Gerber_Files
+mkdir DSSTI02_DXF
+mkdir DSSTI02_ODB
+mkdir DSSTI02_PDF
+cd ..
+mkdir DSSTI02_Mechanical
+mkdir DSSTI02_Reports
+mkdir DSSTI02_Scripts
+cd ..
+mkdir DSSTI02_Schematic
+cd DSSTI02_Schematic
+mkdir DSSTI02_Netlist
+cd ..
+cd ..
+
+:: AUTHOR->DEL: CREATE PROJECT DIRECTORY STRUCTURE LIKE THIS USING PYTHON. GIVE TO JUNIOR ENGINEERS.
